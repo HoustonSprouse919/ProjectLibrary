634d4c1acfeb4018b9b03eacb56ccb3767ce8055
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-10-02 15:09:32 -0400

    some visuals added

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ function book(title, author, pages, read){
     this.author = author
     this.page = pages
     this.readStatus = read
+    
 }
 book.prototype.info = function(){
     return(this.title+" by "+this.author+", "+this.page +" pages, "+this.readStatus+" read");
@@ -15,7 +16,7 @@ function addBookToLibrary(book){
 let bookList = document.createElement("ul");
 
 function displayBooks(){
-    const container = document.querySelector('#container'); 
+const container = document.querySelector('#container'); 
 for(var i = 0; i< myLibrary.length; i++){ //run amount times //goes into container
 const content = document.createElement('div');  //creates divs called content
 content.classList.add('content')
