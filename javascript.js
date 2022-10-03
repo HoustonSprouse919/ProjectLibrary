b4b37e5e0b5f06f6a8318db30dd67c62f686efef
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-10-02 20:10:17 -0400

    adding new books funcationality working with visualization

--- javascript.js
+++ javascript.js
@@ -17,6 +17,7 @@ let bookList = document.createElement("ul");
 
 function displayBooks(){
 const container = document.querySelector('#container'); 
+container.replaceChildren();
 for(var i = 0; i< myLibrary.length; i++){ //run amount times //goes into container
 const content = document.createElement('div');  //creates divs called content
 content.classList.add('content')
@@ -30,6 +31,7 @@ function addNew(){
     var newPages = prompt("please input number of page");
     var newReadStatus = prompt("please input if you have or haven't read");
     new book(newTitle, newAuthor, newPages, newReadStatus);
+    displayBooks();
 }
 
 new book("houston's book", "Houston", "55", "has not");
