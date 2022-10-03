d02b85eea9ef15736317244cce558cb4ab5deead
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-10-03 13:27:41 -0400

    change read status now working

--- javascript.js
+++ javascript.js
@@ -16,30 +16,41 @@ function addBookToLibrary(book){
 let bookList = document.createElement("ul");
 
 function displayBooks(){
-const container = document.querySelector('#container'); 
-container.replaceChildren();
-for(var i = 0; i< myLibrary.length; i++){ //run amount times //goes into container
-const content = document.createElement('div');  //creates divs called content
+const container = document.querySelector('#container'); //selects container we are using
+container.replaceChildren();  //clears the container every time we call it so no duplicates
+for(var i = 0; i< myLibrary.length; i++){ 
+const content = document.createElement('div'); 
 content.classList.add('content')
-content.textContent= myLibrary[i].info()
-var button = document.createElement("BUTTON");
-var text = document.createTextNode("Change read Status");
+content.textContent= myLibrary[i].info() //adds book info
+let button = document.createElement("BUTTON"); //creates button
+let text = document.createTextNode("Change read Status"); //adds text to buttons
 button.appendChild(text);
+button.setAttribute("id", i); //supposed to be id for each button but its the same for all of them
+console.log(button.id)
+button.addEventListener("click", function(){
+     if(myLibrary[button.id].readStatus == "has"){
+        myLibrary[button.id].readStatus = "has not"
+     } else{
+        myLibrary[button.id].readStatus = "has"
+     }
+     displayBooks();
+  });
 container.appendChild(content);
 content.appendChild(button);
     }
 }
 function addNew(){
-    var newTitle = prompt("please input title");
-    var newAuthor = prompt("please input author name");
-    var newPages = prompt("please input number of page");
-    var newReadStatus = prompt("please input if you have or haven't read");
+    let newTitle = prompt("please input title");
+    let newAuthor = prompt("please input author name");
+    let newPages = prompt("please input number of page");
+    let newReadStatus = prompt("please input if you have or haven't read");
     new book(newTitle, newAuthor, newPages, newReadStatus);
     displayBooks();
 }
 
 new book("houston's book", "Houston", "55", "has not");
 new book("Chainsaw man", "Fujimoto", "Alot", "has");
+new book("The Hatchet", "Gray Paulsen", "300", "has");
 
 
 console.log(myLibrary)
