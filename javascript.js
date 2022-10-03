9b3e81336d9479082662ed2ddf8e03f1e92bd349
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-10-03 13:47:46 -0400

    all buttons working correctly, will come back to do forms

--- javascript.js
+++ javascript.js
@@ -1,21 +1,20 @@
-let myLibrary = [];
-function book(title, author, pages, read){
+let myLibrary = []; //array to hold all of our books
+function book(title, author, pages, read){ //constructor for new book objects
     this.title = title
     this.author = author
     this.page = pages
     this.readStatus = read
     addBookToLibrary(this)
 }
-book.prototype.info = function(){
+book.prototype.info = function(){ //function in all books to return the info
     return(this.title+" by "+this.author+", "+this.page +" pages, "+this.readStatus+" read");
 }
 
-function addBookToLibrary(book){
+function addBookToLibrary(book){ //takes a book object and adds it to end of myLibrary array
     myLibrary.push(book)
 }
-let bookList = document.createElement("ul");
 
-function displayBooks(){
+function displayBooks(){ //this is here to show all of our books on the page using dom manipulation
 const container = document.querySelector('#container'); //selects container we are using
 container.replaceChildren();  //clears the container every time we call it so no duplicates
 for(var i = 0; i< myLibrary.length; i++){ 
@@ -31,7 +30,7 @@ button2.appendChild(text2);
 button2.setAttribute("id", i);
 button.setAttribute("id", i); //supposed to be id for each button but its the same for all of them
 console.log(button.id)
-button.addEventListener("click", function(){
+button.addEventListener("click", function(){ //when button is clicked toggles between read and not read
      if(myLibrary[button.id].readStatus == "has"){
         myLibrary[button.id].readStatus = "has not"
      } else{
@@ -40,7 +39,7 @@ button.addEventListener("click", function(){
      displayBooks();
   });
 
-  button2.addEventListener("click", function(){
+  button2.addEventListener("click", function(){ //removes book from list
     myLibrary.splice(button.id,1)
     displayBooks();
  });
@@ -49,7 +48,7 @@ content.appendChild(button);
 content.appendChild(button2);
     }
 }
-function addNew(){
+function addNew(){ //ask user for info on new book
     let newTitle = prompt("please input title");
     let newAuthor = prompt("please input author name");
     let newPages = prompt("please input number of page");
